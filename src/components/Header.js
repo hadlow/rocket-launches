9dee71a4bdc35ef76bd9a1d0350fe55b02b30cff
Billy Hadlow <me@billyhadlow.com>
2022-01-23 15:25:01 +0000

    Header layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+
+import spacexLogo from '../assets/SpaceX-Logo.png'
 
 const Header = () => {
-  return <>
-    <Link to={'/'}>Next launch</Link>
-    <Link to={'/upcoming'}>Upcoming launches</Link>
-  </>
+  return <header>
+    <div className="flex py-4">
+      <img src={spacexLogo} width={200} />
+
+      <NavLink to={'/'} exact={true} className={({ isActive }) => (isActive ? 'ml-auto font-bold' : 'ml-auto')}>Next launch</NavLink>
+      <NavLink to={'/upcoming'} className={({ isActive }) => (isActive ? 'ml-6 font-bold' : 'ml-6')}>Upcoming launches</NavLink>
+    </div>
+  </header>
 }
 
 export default Header
